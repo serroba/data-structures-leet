f8cff1f112589387513baacc6f6a92634ecbd6cf
Sebastian Machuca <sebastian@hachiko.energy>
2025-12-16 17:32:45 +1100

    Add more leet

---
+++ training/1323_maximum_69_test.go
@@ -0,0 +1,28 @@
+package training
+
+import "testing"
+
+func TestMaximum69Number(t *testing.T) {
+	type args struct {
+		num int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "test 6", args: args{num: 6}, want: 9},
+		{name: "test 9", args: args{num: 9}, want: 9},
+		{name: "test 69", args: args{num: 69}, want: 99},
+		{name: "test 9669", args: args{num: 9669}, want: 9969},
+		{name: "test 9996", args: args{num: 9996}, want: 9999},
+		{name: "test 9999", args: args{num: 9999}, want: 9999},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Maximum69Number(tt.args.num); got != tt.want {
+				t.Errorf("Maximum69Number() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ training/342_power_four_test.go
@@ -0,0 +1,27 @@
+package training
+
+import "testing"
+
+func TestIsPowerOfFour(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{name: "test 0", args: args{n: 0}, want: false},
+		{name: "test 1", args: args{n: 1}, want: true},
+		{name: "test 2", args: args{n: 2}, want: false},
+		{name: "test 4", args: args{n: 4}, want: true},
+		{name: "test 16", args: args{n: 16}, want: true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsPowerOfFour(tt.args.n); got != tt.want {
+				t.Errorf("IsPowerOfFour() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
